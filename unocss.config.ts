16b9e6840d23ba4dbb528f8f37300544fdb94ebc
widcardw <lui747353367@qq.com>
2022-07-23 10:53:48 +0800

    styles: update some styles

--- unocss.config.ts
+++ unocss.config.ts
@@ -12,7 +12,7 @@ import {
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'inline-block px-4 py-1 border border-transparent rounded bg-#7f7f7f08 disabled:opacity-20 disabled:bg-zinc-200 dark:disabled:bg-zinc-800 hover:border-#646cff font-500 transition-all'],
+    ['btn', 'inline-block px-4 py-1 border border-transparent rounded bg-#7f7f7f0c disabled:opacity-20 disabled:bg-zinc-200 dark:disabled:bg-zinc-800 hover:border-#646cff font-500 transition-all'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-#646cff !outline-none'],
     ['text-active', 'text-#646cff dark:text-#646cff opacity-100'],
   ],
