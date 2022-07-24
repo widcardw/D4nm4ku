ab5879c3bb907626e89bbbc45cff1e4f981c5425
widcardw <lui747353367@qq.com>
2022-07-24 12:29:21 +0800

    component: img with error handling

---
+++ src/components/img/MyImg.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  src: string
+}>()
+
+const onError = (e: Event) => {
+  (e.target as HTMLImageElement).src = '/noface.gif'
+}
+</script>
+
+<template>
+  <img loading="/noface.gif" w-3rem h-3rem :src="src" rounded-full shadow-md @error="onError">
+  <!-- <img v-if="loading || !imgVisible" w-3rem h-3rem src="/noface.gif"> -->
+</template>
