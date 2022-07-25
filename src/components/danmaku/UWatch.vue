18627ef5ac967f181836e3e548f7460f236226fe
widcardw <lui747353367@qq.com>
2022-07-25 16:57:36 +0800

    feat: add population and fan snumber

---
+++ src/components/danmaku/UWatch.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  population: number
+  fans: string
+}>()
+</script>
+
+<template>
+  <div flex p-2 justify-between>
+    <div>观看人数 {{ population }}</div>
+    <div>粉丝数 {{ fans }}</div>
+  </div>
+</template>
