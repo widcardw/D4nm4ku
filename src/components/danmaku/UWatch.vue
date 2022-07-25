295d3bbe04d8aff80c2065803d47e81917f48aed
widcardw <lui747353367@qq.com>
2022-07-25 23:10:46 +0800

    chore: draggable bar

--- src/components/danmaku/UWatch.vue
+++ src/components/danmaku/UWatch.vue
@@ -6,8 +6,23 @@ defineProps<{
 </script>
 
 <template>
-  <div flex p-2 justify-between>
+  <div
+    flex justify-between
+    p-2
+    data-tauri-drag-region
+    cursor-move
+    class="sel-none"
+  >
     <div>观看人数 {{ population }}</div>
     <div>粉丝数 {{ fans }}</div>
   </div>
 </template>
+
+<style scoped>
+.sel-none {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
