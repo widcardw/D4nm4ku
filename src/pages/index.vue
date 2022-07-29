777a5d490c933382168eda6f70eb2881e8493ebb
widcardw <lui747353367@qq.com>
2022-07-29 20:39:29 +0800

    test: components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import USwitch from '../components/ui/USwitch.vue'
+import UCheckBox from '../components/ui/UCheckBox.vue'
+
+const isChecked = ref(true)
 </script>
 
 <template>
-  <div />
+  <div>
+    <USwitch v-model="isChecked">
+      啊啊啊
+    </USwitch>
+    <UCheckBox v-model="isChecked">
+      必必剥剥
+    </UCheckBox>
+  </div>
 </template>
 
 <route lang="yaml">
