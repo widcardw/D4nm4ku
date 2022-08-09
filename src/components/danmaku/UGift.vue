c546876e30ae784e6c37ab1877cf0369da4746aa
widcardw <lui747353367@qq.com>
2022-08-09 20:01:54 +0800

    chore: re-arrange gift danmaku

--- src/components/danmaku/UGift.vue
+++ src/components/danmaku/UGift.vue
@@ -23,21 +23,27 @@ const store = useStore()
   <div flex w-full space-x-2 bg-blue-800 text-white p-2 rounded my-2>
     <Avatar w-3rem h-3rem :src="face" />
     <div flex-1>
-      <div flex justify-between text-sm font-bold wsn>
-        {{ uname }}
+      <div flex justify-between text-sm>
+        <div font-bold>
+          {{ uname }}
+        </div>
+        <div v-if="store.getConfig.showTime">
+          {{ new Date(ts * 1000).toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+          }) }}
+        </div>
       </div>
-      <div font-bold text-lg wsn>
-        {{ action }} <span :class="{ 'text-amber': coinType === 'gold' }">{{ giftName }}</span> × {{ num }}
+      <div font-bold text-lg flex justify-between items-center>
+        <div>{{ action }} <span :class="{ 'text-amber': coinType === 'gold' }">{{ giftName }}</span> × {{ num }}</div>
+        <div v-if="coinType === 'gold'" text-amber text-sm>
+          ￥{{ price / 1000 }}
+        </div>
       </div>
     </div>
+    <div grid grid-rows-2 />
     <MyImg
       :src="(store.giftInfoList.find(x => x.id === giftId) || { webp: '' }).webp" class="w-3rem h-3rem"
     />
-    <div v-if="store.getConfig.showTime" text-sm wsn>
-      {{ new Date(ts * 1000).toLocaleTimeString('en-US', {
-        hour: '2-digit',
-        minute: '2-digit',
-      }) }}
-    </div>
   </div>
 </template>
