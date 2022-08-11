4804ca5c9ad369c08396afa68ed1643d40451eec
widcardw <lui747353367@qq.com>
2022-08-11 21:37:29 +0800

    chore: css

--- unocss.config.ts
+++ unocss.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     ['btn', 'inline-block px-4 py-1 bg-#646cff bg-opacity-80 disabled:(opacity-20, bg-zinc-500) hover:bg-opacity-100 font-500 transition-all text-white'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-#646cff !outline-none'],
     ['text-active', 'text-#646cff dark:text-#646cff opacity-100'],
-    ['m-input', 'border border-zinc-300 dark:border-zinc-600 !outline-none px-2 py-1 bg-transparent'],
+    ['m-input', 'border border-zinc-300 dark:border-zinc-600 !outline-none px-2 py-1 bg-transparent leading-normal'],
     ['wsn', 'whitespace-nowrap'],
   ],
   presets: [
