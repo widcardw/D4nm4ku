e35afcb75d86cf56d73aff0edfc30e6afdf55790
widcardw <lui747353367@qq.com>
2022-08-11 21:39:23 +0800

    refactor: merge engine into index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,26 +1,58 @@
 <script setup lang="ts">
-const isChecked = ref(true)
-const fruit = ref('banana')
+import { WebviewWindow } from '@tauri-apps/api/window'
+import UInputBtn from '~/components/ui/UInputBtn.vue'
+const roomId = useStorage('roomId', '')
+
+let webview: WebviewWindow | null = null
+const msgRef = inject('msgRef') as any
+const store = useStore()
+const createWebview = () => {
+  if (roomId.value.trim() === '') {
+    msgRef.value.pushMsg({
+      type: 'warning',
+      content: '房间号不能为空！',
+    })
+    return
+  }
+  if (webview) {
+    webview.close()
+    webview = null
+    store.linked = false
+    // eslint-disable-next-line no-console
+    console.log('关闭窗口')
+    return
+  }
+  webview = new WebviewWindow('danmakuWidget', {
+    url: '/show',
+    decorations: false,
+    width: 400,
+    height: 600,
+    transparent: true,
+    alwaysOnTop: true,
+    title: 'D4nm4ku',
+    minWidth: 320,
+    minHeight: 400,
+  })
+  store.linked = true
+}
+const msg = ref('')
 </script>
 
 <template>
-  <div>
-    <USwitch v-model="isChecked" :disabled="false">
-      复选框
-    </USwitch>
-    <UCheckBox v-model="isChecked">
-      Switch
-    </UCheckBox>
-    <div>
-      <URadio v-model="fruit" value="apple" name="fruit">
-        苹果
-      </URadio>
-      <URadio v-model="fruit" value="banana" name="fruit">
-        香蕉
-      </URadio>
-      <URadio v-model="fruit" value="orange" name="fruit">
-        橘子
-      </URadio>
+  <div h-100vh flex flex-col>
+    <div flex-1 flex justify-center>
+      <div self-end text-2xl>
+        D4nm4ku
+      </div>
     </div>
+    <UInputBtn
+      v-model="roomId"
+      self-center
+      :btn-disabled="roomId.trim() === ''"
+      @click-btn="createWebview"
+    >
+      {{ store.linked ? '断开' : '连接' }}
+    </UInputBtn>
+    <div flex-1 />
   </div>
 </template>
