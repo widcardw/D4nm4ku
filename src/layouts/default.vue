2cb99f7216325df27dcaf9a8f01cdfb6997e0d0f
widcardw <lui747353367@qq.com>
2022-08-11 21:44:20 +0800

    style: fix darkmode

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,7 +3,7 @@ import SideBar from '~/components/SideBar.vue'
 </script>
 
 <template>
-  <div class="use-dark" h-100vh>
+  <div class="use-dark">
     <SideBar />
     <div ml-2rem flex-1>
       <RouterView />
