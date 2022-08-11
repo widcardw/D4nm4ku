8dc9342c708b0a06a5135735ead7b69910faf473
widcardw <lui747353367@qq.com>
2022-08-11 21:38:06 +0800

    feat: add material design input text

---
+++ src/components/ui/UMdInput.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  modelValue: string
+  disabled?: boolean
+  title?: string
+}>(), {
+  disabled: false,
+  title: '请输入',
+})
+
+const emits = defineEmits(['update:modelValue'])
+const value = useVModel(props, 'modelValue', emits)
+
+const inputRef = ref<HTMLInputElement>()
+const { focused } = useFocus(inputRef)
+const shouldFloat = computed(() => focused.value || value.value !== '')
+</script>
+
+<template>
+  <div inline-flex m="x-2 y-2">
+    <input
+      ref="inputRef"
+      v-model="value"
+      class="bg-transparent !outline-none leading-loose"
+      border="2px rounded zinc-300 dark:zinc-700 focus:#646cff"
+      p="x-2 y-1"
+      w-15rem
+      :disabled="disabled"
+    >
+    <div
+      absolute transition-all m="x-3 y-10px" op-50
+      :class="{
+        'scale-80 translate-y--20px bg-white dark:bg-#242424 leading-tight !op-100': shouldFloat,
+      }"
+      style="transform-origin: left"
+      @click="focused = !focused"
+    >
+      {{ title }}
+    </div>
+  </div>
+</template>
