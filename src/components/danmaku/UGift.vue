aea2e3c2da2b2ad3a67af0ced52a7d42193c5ddf
widcardw <lui747353367@qq.com>
2022-08-15 20:18:51 +0800

    style: change gift layout

--- src/components/danmaku/UGift.vue
+++ src/components/danmaku/UGift.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Avatar from '~/components/img/Avatar.vue'
 import MyImg from '~/components/img/MyImg.vue'
 import { useStore } from '~/stores/store'
@@ -12,6 +13,7 @@ defineProps<{
   giftName: string
   price: number
   ts: number
+  uid: number
 }>()
 
 const store = useStore()
@@ -22,24 +24,18 @@ const store = useStore()
 
 <template>
   <div flex w-full space-x-2 bg-blue-800 text-white p-2 rounded my-2>
-    <Avatar w-3rem h-3rem :src="face" />
+    <Avatar w-3rem h-3rem :src="face" :uid="uid" :is-blob="false" />
     <div flex-1>
       <div flex justify-between text-sm>
         <div font-bold>
           {{ uname }}
         </div>
-        <div v-if="store.getConfig.showTime">
-          {{ new Date(ts * 1000).toLocaleTimeString('en-US', {
-            hour: '2-digit',
-            minute: '2-digit',
-          }) }}
+        <div v-if="coinType === 'gold'" text-amber text-sm>
+          ￥{{ price / 1000 }}
         </div>
       </div>
       <div font-bold text-lg flex justify-between items-center>
         <div>{{ action }} <span :class="{ 'text-amber': coinType === 'gold' }">{{ giftName }}</span> × {{ num }}</div>
-        <div v-if="coinType === 'gold'" text-amber text-sm>
-          ￥{{ price / 1000 }}
-        </div>
       </div>
     </div>
     <div grid grid-rows-2 />
