580f12b4e53869d1e3795011845f41157fd56f8c
widcardw <lui747353367@qq.com>
2022-08-16 20:15:20 +0800

    try to add click through

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,6 +12,17 @@ use fetch_img::fetch_image;
 use open_app_dir::open_app_img_dir;
 use load_local_img::load_local_image;
 
+// #[cfg(target_os = "macos")]
+// #[macro_use]
+// extern crate objc;
+
+// use tauri::Manager;
+
+// #[derive(Clone, serde::Serialize, serde::Deserialize)]
+// struct ClickTransparentPayload {
+//   enable: bool
+// }
+
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![
@@ -20,6 +31,23 @@ fn main() {
                       open_app_img_dir,
                       load_local_image
                     ])
+    // .setup(|app| {
+    //   let opt_danmaku = app.get_window("danmakuWidget");
+    //   if let Some(danmaku_window) = opt_danmaku {
+    //     let d = danmaku_window.clone();
+    //     danmaku_window.listen("click-transparent", move |event| {
+    //       let payload: ClickTransparentPayload = serde_json::from_str(event.payload().unwrap()).unwrap();
+    //       let _res = d.with_webview(move |webview| {
+    //         #[cfg(target_os = "macos")]
+    //         unsafe {
+    //           let () = msg_send![webview.ns_window(), setIgnoresMouseEvents: payload.enable];
+    //         }
+    //       });
+    //     });
+    //   }
+
+    //   Ok(())
+    // })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
