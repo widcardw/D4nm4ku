c04b9d8d563ea322d85c9e42b727c106e2271ca0
widcardw <lui747353367@qq.com>
2022-08-19 22:06:49 +0800

    fix: add space

--- src/composables/parseFanNumbers.ts
+++ src/composables/parseFanNumbers.ts
@@ -1,9 +1,9 @@
 export default function (num: number) {
   if (num >= 1e8)
-    return `${(num / 1e8).toFixed(2)}亿`
+    return `${(num / 1e8).toFixed(1)} 亿`
 
   if (num >= 10000)
-    return `${(num / 10000).toFixed(1)}万`
+    return `${(num / 10000).toFixed(1)} 万`
 
   return `${num}`
 }
