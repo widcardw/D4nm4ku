711542171e3f56c47a90ae7588d3bc4b97ab22a1
widcardw <lui747353367@qq.com>
2022-08-19 22:07:30 +0800

    style: refactor input btn

--- src/components/ui/UInputBtn.vue
+++ src/components/ui/UInputBtn.vue
@@ -24,26 +24,27 @@ const value = useVModel(props, 'modelValue', emits)
 </script>
 
 <template>
-  <div rounded inline-block :class="{ flex: noBorder }">
-    <input
-      v-model="value"
-      m-input rounded-l
-      class="b-input"
-      border-r-none
-      :disabled="inputDisabled"
-      :placeholder="placeholder"
-      :class="{ 'border-none flex-1': noBorder }"
-      @keydown.ctrl.enter.exact="emitEvent"
+  <div rounded flex :class="{ flex: noBorder }">
+    <div flex-1>
+      <input
+        v-model="value"
+        m-input rounded-l
+        p-1 w-full
+        border-r-none
+        :disabled="inputDisabled"
+        :placeholder="placeholder"
+        :class="{ 'border-none': noBorder }"
+        @keydown.ctrl.enter.exact="emitEvent"
+      >
+    </div>
+    <button
+      btn
+      :class="{ 'border-~ border-#646cff disabled:border-zinc-300/20': !noBorder }"
+      rounded-r
+      :disabled="btnDisabled"
+      @click="emitEvent"
     >
-    <button btn rounded-r :disabled="btnDisabled" @click="emitEvent">
       <slot />
     </button>
   </div>
 </template>
-
-<style scoped>
-.b-input {
-  padding-top: calc(0.25rem - 1px);
-  padding-bottom: calc(0.25rem - 1px);
-}
-</style>
